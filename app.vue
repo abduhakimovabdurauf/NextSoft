3cdb4483a49bdecc51ef166c35c103f3937b9f40
abduhakimovabdurauf <abduhakimovabdurauf402@gmail.com>
2025-06-13 14:27:23 +0500

    add nuxt/image

--- app.vue
+++ app.vue
@@ -11,7 +11,11 @@
       <!-- Qora parda -->
       <div class="absolute inset-0 bg-black bg-opacity-50"></div>
       
-      <img src="/background.jpg" alt="Fon rasmi" class="absolute inset-0 w-full h-full object-cover opacity-50 pointer-events-none z-0" />
+      <NuxtImg
+        src="/background.jpg"
+        alt="Fon rasmi"
+        class="absolute inset-0 w-full h-full object-cover opacity-50 pointer-events-none z-0"
+      />
       <!-- Kontent -->
       <div data-aos="fade-right" class="relative z-20 px-4 sm:px-10 lg:px-20 w-full max-w-7xl text-white">
         <div class="text-left">
